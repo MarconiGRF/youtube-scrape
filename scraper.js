7455d53b3e909ba4bb8d084400c9c842d36ed649
Marconi Gomes <mgrf@cin.ufpe.br>
2021-01-23 14:29:17 -0300

    Modifying core search to address YTGBSS needs

--- scraper.js
+++ scraper.js
@@ -1,71 +1,48 @@
 const request = require('request');
 
-async function youtube(query, key, pageToken) {
-    return new Promise((resolve, reject) => {
+async function youtube(query, limit, type) {
+    return new Promise((resolve) => {
         let json = { results: [], version: require('./package.json').version };
 
-        // Specify YouTube search url
-        if (key) {
-            json["parser"] = "json_format.page_token";
-            json["key"] = key;
-            
-            // Access YouTube search API
-            request.post(`https://www.youtube.com/youtubei/v1/search?key=${key}`, {
-                json: {
-                    context: {
-                        client: {
-                            clientName: "WEB",
-                            clientVersion: "2.20201022.01.01",
-                        },
-                    },
-                    continuation: pageToken
-                },
-            }, (error, response, body) => {
-                if (!error && response.statusCode === 200) {
-                    parseJsonFormat(body.onResponseReceivedCommands[0].appendContinuationItemsAction.continuationItems, json);
-                    return resolve(json);
-                }
-                resolve({ error: error });
-            });
+        const videoFilter = "&sp=EgIQAQ%253D%253D";
+        const playlistFilter = "&sp=EgIQAw%253D%253D"
+        let url = `https://www.youtube.com/results?q=${encodeURIComponent(query)}`;
+
+        if (type === "playlist") {
+            url += playlistFilter;
+        } else {
+            url += videoFilter;
         }
-        else {
-            let url = `https://www.youtube.com/results?q=${encodeURIComponent(query)}`;
 
-            // Access YouTube search
-            request(url, (error, response, html) => {
-                // Check for errors
-                if (!error && response.statusCode === 200) {
-                    json["parser"] = "json_format";
-                    json["key"] = html.match(/"innertubeApiKey":"([^"]*)/)[1];
+        // Access YouTube search
+        request(url, (error, response, html) => {
+            // Check for errors
+            if (!error && response.statusCode === 200) {
 
-                    // Get script json data from html to parse
-                    let data, sectionLists = [];
-                    try {
-                        let match = html.match(/ytInitialData[^{]*(.*"adSafetyReason":[^;]*});/s);
-                        if (match && match.length > 1) {
-                            json["parser"] += ".object_var";
-                        }
-                        else {
-                            json["parser"] += ".original";
-                            match = html.match(/ytInitialData"[^{]*(.*);\s*window\["ytInitialPlayerResponse"\]/s);
-                        }
-                        data = JSON.parse(match[1]);
-                        json["estimatedResults"] = data.estimatedResults || "0";
-                        sectionLists = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents;
+                // Get script json data from html to parse
+                let data, sectionLists = [];
+                try {
+                    let match = html.match(/ytInitialData[^{]*(.*"adSafetyReason":[^;]*});/s);
+                    if (match && match.length > 1) { }
+                    else {
+                        match = html.match(/ytInitialData"[^{]*(.*);\s*window\["ytInitialPlayerResponse"\]/s);
                     }
-                    catch(ex) {
-                        console.error("Failed to parse data:", ex);
-                        console.log(data);
-                    }
+                    data = JSON.parse(match[1]);
+                    sectionLists = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents;
+                }
+                catch(ex) {
+                    console.error("Failed to parse data:", ex);
+                    console.log(data);
+                }
 
-                    // Loop through all objects and parse data according to type
-                    parseJsonFormat(sectionLists, json);
+                // Loop through all objects and parse data according to type
+                parseJsonFormat(sectionLists, json, limit);
+    
+                return resolve(json);
+            }
+            resolve({ error: error });
+        });
         
-                    return resolve(json);
-                }
-                resolve({ error: error });
-            });
-        }
     });
 };
 
@@ -73,34 +50,26 @@ async function youtube(query, key, pageToken) {
  * Parse youtube search results from json sectionList array and add to json result object
  * @param {Array} contents - The array of sectionLists
  * @param {Object} json - The object being returned to caller
+ * @param {Number} limit - The limits of results to be returned
  */
-function parseJsonFormat(contents, json) {
+function parseJsonFormat(contents, json, limit) {
     contents.forEach(sectionList => {
         try {
             if (sectionList.hasOwnProperty("itemSectionRenderer")) {
-                sectionList.itemSectionRenderer.contents.forEach(content => {
+                for (let index = 0; index < limit && index < sectionList.itemSectionRenderer.contents.length; index++) {
                     try {
-                        if (content.hasOwnProperty("channelRenderer")) {
-                            json.results.push(parseChannelRenderer(content.channelRenderer));
+                        if (sectionList.itemSectionRenderer.contents[index].hasOwnProperty("videoRenderer")) {
+                            json.results.push(parseVideoRenderer(sectionList.itemSectionRenderer.contents[index].videoRenderer));
                         }
-                        if (content.hasOwnProperty("videoRenderer")) {
-                            json.results.push(parseVideoRenderer(content.videoRenderer));
-                        }
-                        if (content.hasOwnProperty("radioRenderer")) {
-                            json.results.push(parseRadioRenderer(content.radioRenderer));
-                        }
-                        if (content.hasOwnProperty("playlistRenderer")) {
-                            json.results.push(parsePlaylistRenderer(content.playlistRenderer));
+                        if (sectionList.itemSectionRenderer.contents[index].hasOwnProperty("playlistRenderer")) {
+                            json.results.push(parsePlaylistRenderer(sectionList.itemSectionRenderer.contents[index].playlistRenderer));
                         }
                     }
                     catch(ex) {
                         console.error("Failed to parse renderer:", ex);
                         console.log(content);
                     }
-                });
-            }
-            else if (sectionList.hasOwnProperty("continuationItemRenderer")) {
-                json["nextPageToken"] = sectionList.continuationItemRenderer.continuationEndpoint.continuationCommand.token;
+                }
             }
         }
         catch (ex) {
@@ -111,28 +80,6 @@ function parseJsonFormat(contents, json) {
 }
 
 /**
- * Parse a channelRenderer object from youtube search results
- * @param {object} renderer - The channel renderer
- * @returns object with data to return for this channel
- */
-function parseChannelRenderer(renderer) {
-    let channel = {
-        "id": renderer.channelId,
-        "title": renderer.title.simpleText,
-        "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "snippet": renderer.descriptionSnippet ? renderer.descriptionSnippet.runs.reduce(comb, "") : "",
-        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url,
-        "video_count": renderer.videoCountText ? renderer.videoCountText.runs.reduce(comb, "") : "",
-        "subscriber_count": renderer.subscriberCountText ? renderer.subscriberCountText.simpleText : "0 subscribers",
-        "verified": renderer.ownerBadges &&
-                    renderer.ownerBadges.some(badge => badge.metadataBadgeRenderer.style.indexOf("VERIFIED") > -1) || 
-                    false
-    };
-
-    return { channel };
-}
-
-/**
  * Parse a playlistRenderer object from youtube search results
  * @param {object} renderer - The playlist renderer
  * @returns object with data to return for this playlist
@@ -140,11 +87,11 @@ function parseChannelRenderer(renderer) {
 function parsePlaylistRenderer(renderer) {
     let thumbnails = renderer.thumbnailRenderer.playlistVideoThumbnailRenderer.thumbnail.thumbnails;
     let playlist = {
+        "type": "playlist",
         "id": renderer.playlistId,
         "title": renderer.title.simpleText,
         "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "thumbnail_src": thumbnails[thumbnails.length - 1].url,
-        "video_count": renderer.videoCount
+        "thumbnail_src": thumbnails[thumbnails.length - 1].url
     };
 
     let uploader = {
@@ -152,28 +99,7 @@ function parsePlaylistRenderer(renderer) {
         "url": `https://www.youtube.com${renderer.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url}`
     };
 
-    return { playlist: playlist, uploader: uploader };
-}
-
-/**
- * Parse a radioRenderer object from youtube search results
- * @param {object} renderer - The radio renderer
- * @returns object with data to return for this mix
- */
-function parseRadioRenderer(renderer) {
-    let radio = {
-        "id": renderer.playlistId,
-        "title": renderer.title.simpleText,
-        "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url,
-        "video_count": renderer.videoCountText.runs.reduce(comb, "")
-    };
-
-    let uploader = {
-        "username": renderer.shortBylineText ? renderer.shortBylineText.simpleText : "YouTube"
-    };
-
-    return { radio: radio, uploader: uploader };
+    return { content: playlist, uploader: uploader };
 }
 
 /**
@@ -183,18 +109,12 @@ function parseRadioRenderer(renderer) {
  */
 function parseVideoRenderer(renderer) {
     let video = {
+        "type": "video",
         "id": renderer.videoId,
         "title": renderer.title.runs.reduce(comb, ""),
         "url": `https://www.youtube.com${renderer.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
-        "duration": renderer.lengthText ? renderer.lengthText.simpleText : "Live",
-        "snippet": renderer.descriptionSnippet ?
-                   renderer.descriptionSnippet.runs.reduce((a, b) => a + (b.bold ? `<b>${b.text}</b>` : b.text), ""):
-                   "",
         "upload_date": renderer.publishedTimeText ? renderer.publishedTimeText.simpleText : "Live",
-        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url,
-        "views": renderer.viewCountText ?
-            renderer.viewCountText.simpleText || renderer.viewCountText.runs.reduce(comb, "") :
-            (renderer.publishedTimeText ? "0 views" : "0 watching")
+        "thumbnail_src": renderer.thumbnail.thumbnails[renderer.thumbnail.thumbnails.length - 1].url
     };
 
     let uploader = {
@@ -205,7 +125,7 @@ function parseVideoRenderer(renderer) {
         renderer.ownerBadges.some(badge => badge.metadataBadgeRenderer.style.indexOf("VERIFIED") > -1) || 
         false;
 
-    return { video: video, uploader: uploader };
+    return { content: video, uploader: uploader };
 }
 
 /**
